e104584671f994238c3897999c21ea7983e41cfc
Daniel Andersson <dandersson23@gmail.com>
2017-04-05 22:42:22 +0200

    Initial commit

---
+++ app/src/main/java/com/example/assignment1/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.assignment1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void goToCompass(View view) {
+        Intent intent = new Intent(this, Compass.class);
+        startActivity(intent);
+    }
+
+    public void goToAccelerometer(View view) {
+        Intent intent = new Intent(this, Accelerometer.class);
+        startActivity(intent);
+    }
+
+}
